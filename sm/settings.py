317ddce30e7bed53d5b8fbdf4f77f68c1e8784cd
Pritam Mandal <89613210+hellopritammandal@users.noreply.github.com>
2024-06-03 01:46:09 +0530

    Update settings.py

--- sm/settings.py
+++ sm/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["."]
+ALLOWED_HOSTS = [".vercel.com"]
 
 # Application definition
 
